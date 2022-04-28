fd7ea8d3df69d425d8febee01f89dddc183477b3
kitae12 <77874046+kitae12@users.noreply.github.com>
2022-04-29 00:09:48 +0900

    ?

--- Lecture_1.playground/Contents.swift
+++ Lecture_1.playground/Contents.swift
@@ -20,3 +20,18 @@ else{
     print(str2)
 }
 
+
+
+var topTitle = ("메인화면", "mainIcon.png")
+
+topTitle.0
+topTitle.1
+
+// http status code
+var httpError = (statusCode: 404, description: "not found")
+
+httpError.statusCode
+httpError.description
+
+print(httpError.statusCode)
+print(httpError.description)
