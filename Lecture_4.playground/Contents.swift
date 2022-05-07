c093f901874ea8368d3dc3d08830cc387f5d8627
kitae12 <77874046+kitae12@users.noreply.github.com>
2022-05-07 23:59:22 +0900

    Unicode, String

--- Lecture_4.playground/Contents.swift
+++ Lecture_4.playground/Contents.swift
@@ -1,7 +1,55 @@
 import UIKit
 
-var greeting = "Hello, playground"
-//
-//
-//
-//
+// Unicode
+// 숫자만 입력 받아야 한다.
+let inputValue = "5"
+
+if inputValue >= "\u{30}" && inputValue <= "\u{39}" {
+    print("숫자다")
+}else{
+    print("숫자가 아니다")
+}
+
+
+// 영어만 입력 받아야 한다.
+"\u{41}"
+"\u{7a}"
+
+
+// String
+let myName = "ki tae Park"
+
+for character in myName {
+    print(character)
+}
+
+// String 한줄씩 출력
+let a = "ki\ntae\nPark"
+
+let b =
+"""
+ki
+tae
+Park
+"""
+print(b)
+
+// String 형태로 출력
+let isOn = false
+isOn.description
+
+let myNum = 123
+// myNum.description
+// String(myNum)
+// "my number is \(myNum)"
+
+
+// 문자열 지정 출력
+"my number is \(myNum)".last // 3
+"my number is \(myNum)".dropLast() // 12
+"my number is \(myNum)".dropFirst() // y~
+
+
+// 정수와 소수점 분리
+let num = 12.33
+String(num).split(separator : ".") // "12", "33"
