56ebedde200d8db6dd2c4e8028bee32df06b5829
kitae12 <77874046+kitae12@users.noreply.github.com>
2022-07-04 21:22:02 +0900

    Properties

--- Lecture_11.playground/Contents.swift
+++ Lecture_11.playground/Contents.swift
@@ -1,5 +1,47 @@
 import UIKit
 
-// Properties.
-//.............
-//
+// Properties
+
+
+class MyInfo {
+    
+    // sotred property
+    var name = ""
+    var age = 0
+    
+    // lazy stored property
+    lazy var myProfiles = [UIImage(named: "n"), UIImage(named: "e"),
+                      UIImage(named: "q"), UIImage(named: "s")]
+    
+    // computed property
+    var isAdult: Bool {
+        if age > 19 {
+            return true
+        }
+        return false
+    }
+    
+    // email -> 보안 -> 항상 암호화 된 값으로 사용한다.
+    var _email = ""
+    var email: String {
+        get{
+            return _email
+        }
+        set{
+            _email = newValue.hash.description
+        }
+    }
+}
+
+let myInfo = MyInfo()
+
+myInfo.age = 20
+myInfo.name = "Park"
+myInfo.myProfiles
+myInfo.isAdult
+myInfo.email = "abc@test.com"
+myInfo.email
+
+print(myInfo.age)
+
+
